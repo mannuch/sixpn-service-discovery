86e7eb6d229be8c3b6ee7e1ede91fbda0cca6755
Matthew Mannucci <mannuch31@gmail.com>
2023-01-15 18:23:20 -0800

    Implementing SixPNServiceDiscovery. Added service registration and singular lookup functionality.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "sixpn-service-discovery",
+    platforms: [
+        .macOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -15,7 +18,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/apple/swift-service-discovery.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-atomics.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0")
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/OpenKitten/NioDNS.git", from: "2.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +29,8 @@ let package = Package(
             dependencies: [
                 .product(name: "ServiceDiscovery", package: "swift-service-discovery"),
                 .product(name: "Atomics", package: "swift-atomics"),
-                .product(name: "NIOCore", package: "swift-nio")
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "DNSClient", package: "NioDNS")
             ]
         ),
         .testTarget(
--- Sources/SixPNServiceDiscovery/SixPNServiceDiscovery.swift
+++ Sources/SixPNServiceDiscovery/SixPNServiceDiscovery.swift
@@ -1,19 +1,278 @@
-import Atomics
 import Dispatch
+import DNSClient
 import ServiceDiscovery
+import NIO
+import Atomics
+import Logging
+import struct NIOConcurrencyHelpers.NIOLock
 
-public final class SixPNServiceDiscovery<Service: Hashable, Instance: Hashable>: ServiceDiscovery {
-    public func subscribe(to service: Service, onNext nextResultHandler: @escaping (Result<[Instance], Error>) -> Void, onComplete completionHandler: @escaping (CompletionReason) -> Void) -> CancellationToken {
+public final class SixPNServiceDiscovery: ServiceDiscovery {
+    public typealias Service = SixPNService
+    public typealias Instance = SocketAddress
+    
+    let _defaultLookupTimeout: TimeAmount
+    
+    let logger: Logger
+    
+    private let dnsClient: DNSClient
+    
+    private let eventLoop: EventLoop
+    
+    private var services: [Service: [Instance]] = [:]
+    private var subscriptions: [Subscription] = []
+    
+    private var isShutdown: Bool = false
+    
+    public var defaultLookupTimeout: DispatchTimeInterval {
+        .nanoseconds(Int(_defaultLookupTimeout.nanoseconds))
+    }
+    
+    public init(
+        dnsClient: DNSClient,
+        defaultLookupTimeout: TimeAmount,
+        logger: Logger = .init(label: "swift-service-discovery.sixpn"),
+        on eventLoop: EventLoop
+    ) {
+        self._defaultLookupTimeout = defaultLookupTimeout
+        self.dnsClient = dnsClient
+        self.logger = logger
+        self.eventLoop = eventLoop
+    }
+    
+    public func lookup(_ service: Service, deadline: DispatchTime?, callback: @escaping (Result<[SocketAddress], Error>) -> Void) {
+        let promise = self.eventLoop.makePromise(of: [SocketAddress].self)
+        self.lookup(service, deadline: deadline?.toNIODeadline())
+            .cascade(to: promise)
+        
+        
+        let deadlineTask = self.eventLoop.scheduleTask(deadline: deadline?.toNIODeadline() ?? .now() + self._defaultLookupTimeout) {
+            promise.fail(LookupError.timedOut)
+        }
+        
+        promise.futureResult
+            .always { _ in deadlineTask.cancel() }
+            .whenComplete(callback)
+    }
+    
+    public func subscribe(to service: Service, onNext nextResultHandler: @escaping (Result<[SocketAddress], Error>) -> Void, onComplete completionHandler: @escaping (CompletionReason) -> Void) -> CancellationToken {
+        return CancellationToken()
+    }
+    
+    func lookup(_ service: Service, deadline: NIODeadline?) -> EventLoopFuture<[SocketAddress]> {
+        // dispatch to event loop thread if necessary
+        guard self.eventLoop.inEventLoop else {
+            return self.eventLoop.flatSubmit {
+                self.lookup(service, deadline: deadline)
+            }
+        }
+        
+        guard !self.isShutdown else {
+            return self.eventLoop.makeFailedFuture(ServiceDiscoveryError.unavailable)
+        }
+        
+        guard let instances = self.services[service] else {
+            return self.eventLoop.makeFailedFuture(LookupError.unknownService)
+        }
+        
+        return self.eventLoop.makeSucceededFuture(instances)
+    }
+    
+    public func findAndRegister(services: [SixPNService]) -> EventLoopFuture<Void> {
+        // dispatch to event loop thread if necessary
+        guard self.eventLoop.inEventLoop else {
+            return self.eventLoop.flatSubmit {
+                self.findAndRegister(services: services)
+            }
+        }
+        
+        guard !self.isShutdown else {
+            return self.eventLoop.makeFailedFuture(SixPNError.shutdown)
+        }
+        
+        
+        let promise = self.eventLoop.makePromise(of: Void.self)
+        
+        self.queryAllSixPNApps(services)
+            .hop(to: self.eventLoop)
+            .map { [weak self] in
+                // Query AAAA records for socket addresses of services
+                guard let self else {
+                    promise.succeed(())
+                    return
+                }
+                var queries = [EventLoopFuture<Void>]()
+                for service in services {
+                    queries.append(self.queryService(service))
+                }
+                EventLoopFuture.andAllSucceed(queries, promise: promise)
+            }.cascadeFailure(to: promise)
+        
+        return promise.futureResult
+    }
+    
+    private func queryService(_ service: SixPNService) -> EventLoopFuture<Void> {
+        self.logger.info("Looking for top \(service.topNClosestInstances) closest instances of service ['\(service.appName)', port: \(service.port)]")
+        
+        return self.dnsClient.initiateAAAAQuery(
+            host: "top\(service.topNClosestInstances).nearest.of.\(service.appName).internal",
+            port: Int(service.port)
+        )
+        .hop(to: self.eventLoop)
+        .map { [weak self] socketAddresses in
+            guard let self else { return }
+            
+            self.logger.info("Found \(socketAddresses.count) instances of service ['\(service.appName)', port: \(service.port)]")
+            
+            self.services[service] = socketAddresses
+        }
+    }
+    
+    private func queryAllSixPNApps(_ services: [SixPNService]) -> EventLoopFuture<Void> {
+        let promise = self.eventLoop.makePromise(of: Void.self)
+        
+        logger.info("Querying DNS for all SixPN apps in Fly.io organization...")
+        
+        promise.futureResult.whenComplete { [weak self] result in
+            switch result {
+            case .success:
+                self?.logger.info("Found all queried service apps in SixPN network")
+            case .failure(let error):
+                self?.logger.error("Error querying service apps in SixPN network: '\(error)'")
+            }
+        }
+        
+        self.dnsClient.sendQuery(forHost: "_apps.internal", type: .txt).whenComplete { result in
+            guard case let .success(message) = result else {
+                if case let .failure(error) = result {
+                    promise.fail(error)
+                }
+                return
+            }
+            
+            
+            guard !message.answers.isEmpty else {
+                promise.fail(
+                    SixPNError.txtRecordLookup(message: "The DNS Query message did not have any answers.")
+                )
+                return
+            }
+            
+            guard case let .txt(txtRecord) = message.answers[0] else {
+                promise.fail(
+                    SixPNError.txtRecordLookup(message: "The first answer of the DNS Query message was not a TXT record.")
+                )
+                return
+            }
+            
+            let appNames = txtRecord.resource.rawValues[0].split(separator: ",").map { String($0) }
+            
+            var notFound = [SixPNService]()
+            
+            services.forEach { service in
+                if !appNames.contains(service.appName) {
+                    notFound.append(service)
+                }
+            }
+            
+            guard notFound.isEmpty else {
+                promise.fail(SixPNError.servicesNotFound(notFound))
+                return
+            }
+            
+            promise.succeed(())
+        }
+        
+        return promise.futureResult
         
     }
     
-    public func lookup(_ service: Service, deadline: DispatchTime?, callback: @escaping (Result<[Instance], Error>) -> Void) {
+    public func shutdown() -> EventLoopFuture<Void> {
+        // dispatch to event loop thread if necessary
+        guard self.eventLoop.inEventLoop else {
+            return self.eventLoop.flatSubmit {
+                return self.shutdown()
+            }
+        }
         
+        // check to make sure we aren't double closing
+        guard !self.isShutdown else {
+            return self.eventLoop.makeSucceededFuture(())
+        }
+        
+        self.isShutdown = true
+        self.dnsClient.cancelQueries()
+        self.logger.trace("SixPNServiceDiscovery is shutting down, closing all active queries and subscriptions on this event loop")
+        
+        // no locks needed as this can only happen once
+        self.subscriptions
+            .filter { !$0.cancellationToken.isCancelled }
+            .forEach { $0.completionHandler(.serviceDiscoveryUnavailable) }
+        
+        return self.eventLoop.makeSucceededVoidFuture()
     }
     
+    deinit {
+        if !self.isShutdown {
+            assertionFailure("SixPNServiceDiscovery.shutdown() was not called before deinit.")
+        }
+    }
     
-    public var defaultLookupTimeout: DispatchTimeInterval
-    
-
-    public init() {}
+    private struct Subscription {
+            let nextResultHandler: (Result<[Instance], Error>) -> Void
+            let completionHandler: (CompletionReason) -> Void
+            let cancellationToken: CancellationToken
+    }
 }
+
+extension SixPNServiceDiscovery {
+    public func findAndRegister(services: [SixPNService]) async throws {
+        return try await self.findAndRegister(services: services).get()
+    }
+}
+
+extension SixPNServiceDiscovery {
+    public enum SixPNError: Error, CustomStringConvertible {
+        case unknown
+        case txtRecordLookup(message: String)
+        case servicesNotFound([SixPNService])
+        case shutdown
+        
+        public var description: String {
+            switch self {
+            case .unknown:
+                return "Unknown"
+            case .shutdown:
+                return "SixPNServiceDiscovery is shutdown"
+            case let .servicesNotFound(notFoundServices):
+                return "Could not find services: \(notFoundServices.map { $0.description })"
+            case let .txtRecordLookup(message):
+                return "Error during TXT record DNS query: '\(message)'"
+            }
+        }
+    }
+}
+
+public struct SixPNService: Hashable {
+    public let appName: String
+    public let port: UInt
+    public let topNClosestInstances: UInt = 1
+    
+    public var description: String {
+        "\(appName)"
+    }
+    
+    public init(
+        appName: String,
+        port: UInt
+    ) {
+        self.appName = appName
+        self.port = port
+    }
+}
+
+extension DispatchTime {
+    func toNIODeadline() -> NIODeadline {
+        NIODeadline.uptimeNanoseconds(self.uptimeNanoseconds)
+    }
+}
+
